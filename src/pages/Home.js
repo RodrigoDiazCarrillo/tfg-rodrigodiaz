f93f4d68628cd1852b2ec37b9ff3039d73778023
Rodrigo Diaz Carrillo <dzcll90@gmail.com>
2022-10-23 15:36:04 +0200

    homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,9 @@ import "./Home.css";
 import { Carousel } from "../components/Carousel";
 import { Menu } from "../components/Menu";
 import { Footer } from "../components/Footer";
+import { Link} from "react-router-dom";
 import img1 from "../img/418831.jpg";
+import img2 from "../img/5022456.jpg";
 
 const Home = () => {
 
@@ -11,19 +13,42 @@ const Home = () => {
 
     <section className="home">
       <Menu />
-      <Carousel />
-      <div className='carousel-margin'></div>
-      
       <div className="button-container">
       <img src={img1} alt="" className="img1" />
-      <div>
-      <h2>Mapple Guitars</h2>
-      <h2>Desde 2012 creando sonidos de forma sostenible</h2>
-      <button>Más info</button>
+      <div className='div-1'>
+      <h3>Mapple Guitars</h3>
+      <h2>Creamos sonidos de forma sostenible desde 2012</h2>
+      <div className='btn-box'>
+      <Link to="/store">
+      <button className='button-1'>+ Info</button>
+      </Link>
+      </div>
+      </div>
+      </div>
+      <div className='div-2'>
+      <h3>Diseñamos y creamos instrumentos innovadores y de calidad</h3>
+      <h2>10 años de experiencia en el sector</h2>
+         <div className='btn-box2'>
+      <Link to="/about">
+      <button className='button-2'>Sobre nosotros</button>
+      </Link>
+      </div>
+      </div>
+      <div className='div-3'>
+      <img src={img2} alt="" className="img2" />
+      <div className='div-3_text'>
+      <h3>Fomentamos la cultura sostenible con maderas <strong>alternativas</strong></h3>
+         <div >
+         <div className='btn-box3'>
+      <Link to="/woods">
+      <button className='button-1'>Maderas</button>
+      </Link>
+      </div>
       </div>
       </div>
       
-      
+      </div>
+
       <p></p>
        <Footer/> 
     </section>
