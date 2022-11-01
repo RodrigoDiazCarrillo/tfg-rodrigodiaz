2d54ea84accbd744b357d372b5311738e51c5bd6
Rodrigo Diaz Carrillo <dzcll90@gmail.com>
2022-11-01 17:52:19 +0100

    get data from db

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -6,39 +6,118 @@ import { Menu } from "../components/Menu";
 import { Footer } from "../components/Footer";
 import { auth } from "../firebase-config";
 ////////////////////
-import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { getFirestore,getDocs,collection,doc,getDoc} from "firebase/firestore";
+import { get } from "firebase/database";
 
 
 
 
 
 const Store = () => {
-  const [data,setData] = useState({})
+ const [data,setData] = useState([])
+ const [dat,setDat] = useState([])
 
-useEffect(() =>{
-    const querydb = getFirestore();
-    const queryDoc = doc(querydb, 'productos','maderas');
-    getDoc(queryDoc)
-    .then(res => setData(res.data(),  console.log(JSON.stringify(data))))
-    localStorage.setItem('productos', JSON.stringify(data))
+// useEffect(() =>{
+//     const querydb = getFirestore();
+//     const queryDoc = doc(querydb, 'productos','maderas');
+//     getDoc(queryDoc)
+//     .then(res => setData(res.data()
+//     ,  console.log(JSON.stringify(data))
+//     ))
+//     // localStorage.setItem('productos', JSON.stringify(data))
+// },[]) 
 
-    
-},[])
+const prod = async (e) => {
 
+
+  const db = getFirestore();
+  const colRef = collection(db, "cuerdas");
+
+  try {
+    const documents = await getDocs(colRef);
+
+    if(data.length < documents.docs.length) { 
+      documents.forEach(doc => {
+          setData(current => [...current,doc.id]) 
+          setDat(current => [...current,doc.data()]) 
+       })     }
+
+     
+       
+} catch (error) {
+    console.log("error");
+}
+};
+
+
+useEffect(() => { 
+ prod();
+ 
+ }, [])
+
+ 
   return (
     <section className="store">
     <Menu/>
     <section className="products">
-      {
+      {/* {
       Object.entries(data).map(([key, val], i) => (
         <div className="product-card">
-            <h2>{key}</h2>
             <img src ={val}/>
+            <h2>{key}</h2>
+            
         </div> 
         ))
-    
+      } */}
+      
+     
+      {/* {
+      dat.map((d) =>  
+         <li>{d}</li>   
+        
+      )
+      } */}
+      {
+        console.log(dat)
       }
+        
    
+      {/* {data[1] != undefined? 
+      
+      Object.entries(data).map(([key, val], i) => (
+        <div className="product-card">
+            <p>{val}</p>
+             <h2>{key}</h2>
+             <p>{i}</p>
+             
+         </div> 
+          ))
+         :
+         console.log(data[0])
+      } */}
+       
+       {data != undefined ?
+      dat.map((d) =>  
+      <div className="product-card">
+        <p>{d.tipo}</p>   
+        <p>{d.marca}</p>   
+        <img src ={d.imagen}/>
+         <p>{d.modelo}</p>   
+         <p>{d.descripcion}</p>   
+         <p>{d.precio}</p>  
+         </div>
+      )
+      :
+
+      <p className="cargando">Cargando</p>
+  
+
+      } 
+      
+
+ 
+
+
     </section>
     <Footer/>
     </section>
