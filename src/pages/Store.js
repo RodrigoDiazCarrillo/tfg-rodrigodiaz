73b9aa0fcc6437a274f71fbfd3c244c6fcd62e95
Rodrigo Diaz Carrillo <dzcll90@gmail.com>
2022-11-10 07:55:17 +0100

    basket definition

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -6,76 +6,87 @@ import { Menu } from "../components/Menu";
 import { Footer } from "../components/Footer";
 import { auth } from "../firebase-config";
 ////////////////////
-import { getFirestore,getDocs,collection,doc,getDoc} from "firebase/firestore";
+import { getFirestore, getDocs, collection, doc, getDoc, query, where } from "firebase/firestore";
 import { Link } from "react-router-dom";
 
 
 
 
 
-function Store () {
- const [data,setData] = useState([])
- const [dat,setDat] = useState([])
+function Store() {
+  const [data, setData] = useState([])
+  const [dat, setDat] = useState([])
+  const [basket, setBasket] = useState([])
 
 
-// useEffect(() =>{
-//     const querydb = getFirestore();
-//     const queryDoc = doc(querydb, 'productos','maderas');
-//     getDoc(queryDoc)
-//     .then(res => setData(res.data()
-//     ,  console.log(JSON.stringify(data))
-//     ))
-//     // localStorage.setItem('productos', JSON.stringify(data))
-// },[]) 
-
-const prod = async (e) => {
-  const db = getFirestore();
-  const colRef = collection(db, "productos");
-  try {
-    const documents = await getDocs(colRef);
-
-    if(data.length < documents.docs.length) { 
-      documents.forEach(doc => {
-          setData(current => [...current,doc.id]) 
-          setDat(current => [...current,doc.data()]) 
-       })     }     
-} catch (error) {
-    console.log("error");
-}
-};
 
 
-useEffect(() => { 
- prod();
- 
- }, [])
+  // useEffect(() =>{
+  //     const querydb = getFirestore();
+  //     const queryDoc = doc(querydb, 'productos','maderas');
+  //     getDoc(queryDoc)
+  //     .then(res => setData(res.data()
+  //     ,  console.log(JSON.stringify(data))
+  //     ))
+  //     // localStorage.setItem('productos', JSON.stringify(data))
+  // },[]) 
 
- 
+  const prod = async (e) => {
+    const db = getFirestore();
+    const colRef = collection(db, "productos");
+    try {
+      const documents = await getDocs(colRef);
+
+      if (data.length < documents.docs.length) {
+        documents.forEach(doc => {
+          setData(current => [...current, doc.id])
+          setDat(current => [...current, doc.data()])
+        })
+      }
+    } catch (error) {
+      console.log("error");
+    }
+  };
+  useEffect(() => {
+    prod();
+
+  }, [])
+  useEffect(() => {
+    console.log(basket);
+  }, [basket])
+
+
   return (
     <section className="store">
-    <Menu/>
-    <section className="products">
- 
-       {data != undefined ?
-      dat.map((d) =>  
-      <Link className={"linkproduct"}to={d.url}>
-      <div className="product-card">
-        <h2> {d.marca} &nbsp;{d.modelo} </h2>   
-        <img src ={d.imagen}/>
-         <p>{d.precio}</p>  
-         
-         </div>
-         </Link>
-      )
-      
-      :
+      <Menu />
+      <section className="products">
 
-      <p className="cargando">Cargando</p>
-      } 
-      
+        {data != undefined ?
+          dat.map((d, index) =>
+            <div className="product-card">
+              <div className={index}>
+                <Link className={"linkproduct"} to={d.url}>
+                  <h2> {d.marca} &nbsp;{d.modelo} </h2>
+                  <img src={d.imagen} />
+                  <p>{d.precio}</p>
+                </Link>
 
-    </section>
-    <Footer/>
+              </div>
+              <button value={index} onClick={(e) =>
+                setBasket(current => [...current, [e.target.value, dat[e.target.value].precio, dat[e.target.value].marca]])
+
+              }>Añadir a la cesta</button>
+            </div>
+          )
+
+          :
+
+          <p className="cargando">Cargando</p>
+        }
+
+
+      </section>
+      <Footer />
     </section>
   );
 };
